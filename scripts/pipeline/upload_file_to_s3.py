33f684465803dd8cbd2ab47ea19345c422a2ce01
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2024-06-12 11:43:54 -0300

    adjust validation to sys argv

--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -2,8 +2,8 @@ import boto3
 import sys
 
 def main():
-  print(len(sys.argv))
-  if (len(sys.argv) <= 6):
+  print(sys.argv)
+  if (len(sys.argv) < 6):
     print('Error: Required 5 arguments.')
     # Checks for 6 because the script path is in position 0. So len is 6
     # for 5 arguments.
