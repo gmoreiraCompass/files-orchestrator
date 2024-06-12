458dde5b08837ddcc6a1d3855dbb492624fcca1f
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2024-06-12 11:40:58 -0300

    adjust in print argv

--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -2,7 +2,7 @@ import boto3
 import sys
 
 def main():
-  print len(sys.argv)
+  print(len(sys.argv))
   if (len(sys.argv) <= 6):
     print('Error: Required 5 arguments.')
     # Checks for 6 because the script path is in position 0. So len is 6
