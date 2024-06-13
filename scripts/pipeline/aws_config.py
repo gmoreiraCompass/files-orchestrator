654895121acfe136bdc78b070550910da52eda9b
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2024-06-13 11:48:35 -0300

    add 2 points in def

--- scripts/pipeline/aws_config.py
+++ scripts/pipeline/aws_config.py
@@ -2,12 +2,12 @@ import boto3
 
 class Config(aws_access_key, aws_access_secret):
   @staticmethod
-  def session(aws_access_key, aws_access_secret)
+  def session(aws_access_key, aws_access_secret):
     return boto3.Session(
       aws_access_key_id=aws_access_key,
       aws_secret_access_key=aws_access_secret
     )
 
   @staticmethod
-  def clientS3()
+  def clientS3():
     return Config.session.client('s3')
