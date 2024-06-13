df87f7c0106aef5547d93515fa20d33ece152e4d
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2024-06-13 17:21:09 -0300

    change pass props

--- scripts/pipeline/aws_config.py
+++ scripts/pipeline/aws_config.py
@@ -2,11 +2,11 @@ import boto3
 
 class Config:
   def __init__(self, aws_access_key, aws_access_secret):
-    self.aws_access_key = ""
-    self.aws_access_secret = ""
+    self.aws_access_key = aws_access_key
+    self.aws_access_secret = aws_access_secret
 
   @staticmethod
-  def session(self):
+  def session():
     return boto3.Session(
       aws_access_key_id = self.aws_access_key,
       aws_secret_access_key = self.aws_access_secret
