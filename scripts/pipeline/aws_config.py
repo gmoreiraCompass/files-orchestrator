5f6de4d97ef92c1e23cc4465159c223129ca626a
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2024-06-13 11:51:26 -0300

    remove param in session config

--- scripts/pipeline/aws_config.py
+++ scripts/pipeline/aws_config.py
@@ -2,7 +2,7 @@ import boto3
 
 class Config(aws_access_key, aws_access_secret):
   @staticmethod
-  def session(aws_access_key, aws_access_secret):
+  def session():
     return boto3.Session(
       aws_access_key_id=aws_access_key,
       aws_secret_access_key=aws_access_secret
