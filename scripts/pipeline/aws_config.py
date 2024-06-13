1ee09841b67e2bafb8cbbe4406fa4c88dc3f4edd
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2024-06-13 17:35:23 -0300

    remove init in class config

--- scripts/pipeline/aws_config.py
+++ scripts/pipeline/aws_config.py
@@ -1,13 +1,10 @@
 import boto3
 
-class Config:
-  def __init__(self, aws_access_key, aws_access_secret):
-    self.aws_access_key = aws_access_key
-    self.aws_access_secret = aws_access_secret
 
-  @staticmethod
-  def session():
-    return boto3.Session(
-      aws_access_key_id = aws_access_key,
-      aws_secret_access_key = aws_access_secret
-    )
+class Config:
+    @staticmethod
+    def session(aws_access_key, aws_access_secret):
+        return boto3.Session(
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_access_secret
+        )
--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -1,28 +1,29 @@
 import sys
 from aws_config import Config
 
+
 def main():
-  print(sys.argv)
-  if (len(sys.argv) < 6):
-    print('Error: Required 5 arguments.')
-    # Checks for 6 because the script path is in position 0. So len is 6
-    # for 5 arguments.
-    sys.exit(1)
-  
-  bucket_name=sys.argv[1]
-  aws_key=sys.argv[2]
-  aws_access_key=sys.argv[3]
-  aws_access_secret=sys.argv[4]
-  local_path=sys.argv[5]
+    print(sys.argv)
+    if len(sys.argv) < 6:
+        print('Error: Required 5 arguments.')
+        # Checks for 6 because the script path is in position 0. So len is 6
+        # for 5 arguments.
+        sys.exit(1)
 
-  Config(aws_access_key, aws_access_secret)
-  clientS3 = Config.session().client('s3')
+    bucket_name = sys.argv[1]
+    aws_key = sys.argv[2]
+    aws_access_key = sys.argv[3]
+    aws_access_secret = sys.argv[4]
+    local_path = sys.argv[5]
 
-  response = client.upload_file(
-    Filename=local_path,
-    Bucket=bucket_name,
-    Key=aws_key
-  )
-  print('Done uploading')
+    client_s3 = Config.session(aws_access_key, aws_access_secret).client('s3')
+
+    response = client_s3.upload_file(
+        Filename=local_path,
+        Bucket=bucket_name,
+        Key=aws_key
+    )
+    print('Done uploading', response)
+
 
 main()
